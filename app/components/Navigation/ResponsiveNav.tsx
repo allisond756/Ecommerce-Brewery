f8979b9c4e1d7e84f8670f2c1fd893d67a8b6530
Allison Daniel <allisond756@gmail.com>
2024-06-07 13:46:17 -0500

    add navigation

---
+++ app/components/Navigation/ResponsiveNav.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from "./Nav"
+import MoblieNav from "./MoblieNav"
+
+const ResponsiveNav = () => {
+    return (
+        <div>
+            <div>
+                <Nav />
+                <MoblieNav />
+            </div>
+        </div>
+    )
+}
+
+export default ResponsiveNav
