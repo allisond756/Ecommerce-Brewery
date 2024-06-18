8f64be647828f9ddba699ce6eeaddaf28e718032
Allison Daniel <allisond756@gmail.com>
2024-06-18 10:28:32 -0500

    newsletter

--- app/components/Newsletter/Newsletter.tsx
+++ app/components/Newsletter/Newsletter.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Newsletter = () => {
-  return (
-    <div>Newsletter</div>
-  )
+    return (
+        <div className="pt-[5rem] pb-[5rem]">
+            <div className="text-center">
+                <h1 className="text-[38px] sm:text-[28px] md:text-[45px] text-black font-bold uppercase">
+                    Join our Email List for Exclusive Discounts!
+                </h1>
+                <p className="text-black text-opacity-70 w-[85%] md:w-[60%] mx-auto">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Ad quae repudiandae inventore qui minima voluptatem eum autem eveniet beatae est.
+                </p>
+                <div className="flex items-center space-x-2">
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Newsletter
