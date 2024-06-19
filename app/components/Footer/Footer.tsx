a4ac2bd755da78dfc61e91b474e259aa3eafe9ac
Allison Daniel <allisond756@gmail.com>
2024-06-19 09:49:36 -0500

    quick links

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
     return (
         <div className="pt-[5rem] pb-[3rem] bg-gray-900">
             <div className="w-[80%] pb-[2rem] border-b-[2px] border-b-gray-300 border-opacity-50 mx-auto grid grid-cols-1
-             md:grid-cols-2 lg:grid-cols-4 gap-[3rem] items-start">
+             md:grid-cols-3 lg:grid-cols-4 gap-[3rem] items-start">
                 {/* site logo */}
                 <div>
                     <div className="flex items-center text-white space-x-1">
-                        <RiBeerFill className="w-[1.2rem] h-[1.2rem] sm:w-[1.4rem] sm:h-[1.4rem] text-amber-500" />
-                        <h1 className="text-[20px] sm:text-[30px] font-semibold">
+                        <RiBeerFill className="w-[1.5rem] h-[1.5rem] sm:w-[1.5rem] sm:h-[1.5rem] text-amber-500" />
+                        <h1 className="text-[20px] sm:text-[20px] font-semibold">
                             BismuthBrewing
                         </h1>
                     </div>
@@ -24,8 +24,9 @@ const Footer = () => {
                         +(123)-456-7890
                     </p>
                 </div>
+                {/* footer products */}
                 <div>
-                    <h1 className="text-[23px] font-semibold mb-[2rem] text-white">Products</h1>
+                    <h1 className="text-[20px] font-semibold mb-[2rem] text-white">Products</h1>
                     <p className="mt-[0.7rem] text-white opacity-60 hover:text-yellow-300 transition-all 
                      duration-200 cursor-pointer w-fit">
                         Pale Ale
@@ -42,7 +43,26 @@ const Footer = () => {
                      duration-200 cursor-pointer w-fit">
                         Stout
                     </p>
-                    
+                </div>
+                {/* quick links */}
+                <div>
+                    <h1 className="text-[20px] font-semibold mb-[2rem] text-white">Quick Links</h1>
+                    <p className="mt-[0.7rem] text-white opacity-60 hover:text-yellow-300 transition-all 
+                     duration-200 cursor-pointer w-fit">
+                        Home
+                    </p>
+                    <p className="mt-[0.7rem] text-white opacity-60 hover:text-yellow-300 transition-all 
+                     duration-200 cursor-pointer w-fit">
+                        Shop
+                    </p>
+                    <p className="mt-[0.7rem] text-white opacity-60 hover:text-yellow-300 transition-all 
+                     duration-200 cursor-pointer w-fit">
+                        Menu
+                    </p>
+                    <p className="mt-[0.7rem] text-white opacity-60 hover:text-yellow-300 transition-all 
+                     duration-200 cursor-pointer w-fit">
+                        Contact
+                    </p>
                 </div>
             </div>
         </div>
